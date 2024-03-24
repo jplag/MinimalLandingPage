88677fa472702b8ffeddf6639bec40f8b6f1c664
Alexander Vogt <alexander_vogt@outlook.de>
2024-03-24 21:36:44 +0100

    header spacing

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,8 @@
 <template>
-  <header class="sticky top-0 h-10 overflow-hidden shadow bg-white"
+  <header class="sticky top-0 h-14 overflow-hidden shadow bg-white"
   >
     <img
-      class="max-h-full ml-2 md:mx-auto"
+      class="max-h-full ml-2 py-2 md:mx-auto"
       src="https://raw.githubusercontent.com/jplag/JPlag/develop/report-viewer/src/assets/jplag-dark-transparent.png"
       alt="Header image"
     />
