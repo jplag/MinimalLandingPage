2711ec34aae08da2fa6a6b402161d636c4ab3825
Alex | Kronox <alexander_vogt@outlook.de>
2024-03-25 09:23:34 +0100

    fix footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col min-h-screen">
     <HeaderComponent class="w-full mt-0" />
     <main class="flex flex-col items-center px-4 flex-1 py-5">
       <ReferenceSection />
       <hr class="my-5 w-full">
       <PublicationsSection class="md:max-w-[90%]" />
     </main>
-    <footer class="shadow border-t border-gray-100 py-2">
+    <footer class="shadow border-t border-gray-100 py-2 relative bottom-0">
       <p class="ml-2">
         &copy; 2024 JPlag
       </p>
