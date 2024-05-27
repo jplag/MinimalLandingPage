2a5deea2fca76161065e8350afa8b86e276ca3fd
Timur Sağlam <timur.saglam@kit.edu>
2024-05-27 13:56:59 +0200

    Unify ICSE naming.

--- src/components/PublicationsSection.vue
+++ src/components/PublicationsSection.vue
@@ -22,14 +22,14 @@ const publicationLinks: PublicationsLink[] = [
     title: 'Obfuscation-Resilient Software Plagiarism Detection with JPlag',
     authors: ['Timur Sağlam', 'Sebastian Hahner', 'Larissa Schmid', 'Erik Burger'],
     venue:
-      '2024 IEEE/ACM 46th International Conference on Software Engineering: Companion Proceedings (ICSE-C ’24)',
+      'IEEE/ACM 46th International Conference on Software Engineering: Companion Proceedings (ICSE-C ’24)',
     year: 2024
   },
   {
     url: 'https://doi.org/10.1145/3597503.3639192',
     title: 'Detecting Automatic Software Plagiarism via Token Sequence Normalization',
     authors: ['Timur Sağlam', 'Moritz Brödel', 'Larissa Schmid', 'Sebastian Hahner'],
-    venue: '2024 IEEE/ACM 46th International Conference on Software Engineering (ICSE ’24)',
+    venue: 'IEEE/ACM 46th International Conference on Software Engineering (ICSE ’24)',
     year: 2024
   },
   {
@@ -37,7 +37,7 @@ const publicationLinks: PublicationsLink[] = [
     title: 'Automated Detection of AI-Obfuscated Plagiarism in Modeling Assignments',
     authors: ['Timur Sağlam', 'Sebastian Hahner', 'Larissa Schmid', 'Erik Burger'],
     venue:
-      '46th International Conference on Software Engineering: Software Engineering Education and Training (ICSE-SEET ’24)',
+      'IEEE/ACM 46th International Conference on Software Engineering: Software Engineering Education and Training (ICSE-SEET ’24)',
     year: 2024
   },
   {
