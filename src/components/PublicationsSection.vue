b7e86c8251127dfa7a4f4e8b60f906b9c2fe7495
Timur Sağlam <timur.saglam@kit.edu>
2025-02-21 13:20:09 +0100

    Fix publication metadata.

--- src/components/PublicationsSection.vue
+++ src/components/PublicationsSection.vue
@@ -31,7 +31,7 @@ const publicationLinks: PublicationsLink[] = [
     title: 'Mitigating Obfuscation Attacks on Software Plagiarism Detectors via Subsequence Merging',
     authors: ['Timur Sağlam', 'Nils Niehues', 'Sebastian Hahner', 'Larissa Schmid'],
     venue:
-      'IEEE 37th Conference on Software Engineering Education and Training (CSEET ’25)',
+      'IEEE 37th Conference on Software Engineering Education and Training (CSEET)',
     year: 2025,
     doi: '10.5445/IR/1000179016'
   },
@@ -40,7 +40,7 @@ const publicationLinks: PublicationsLink[] = [
     title: 'Obfuscation-Resilient Software Plagiarism Detection with JPlag',
     authors: ['Timur Sağlam', 'Sebastian Hahner', 'Larissa Schmid', 'Erik Burger'],
     venue:
-      'IEEE/ACM 46th International Conference on Software Engineering: Companion Proceedings (ICSE-C ’24)',
+      'IEEE/ACM 46th International Conference on Software Engineering: Companion Proceedings (ICSE-C)',
     year: 2024,
     doi: '10.1145/3639478.3643074'
   },
@@ -48,7 +48,7 @@ const publicationLinks: PublicationsLink[] = [
     url: 'https://doi.org/10.1145/3597503.3639192',
     title: 'Detecting Automatic Software Plagiarism via Token Sequence Normalization',
     authors: ['Timur Sağlam', 'Moritz Brödel', 'Larissa Schmid', 'Sebastian Hahner'],
-    venue: 'IEEE/ACM 46th International Conference on Software Engineering (ICSE ’24)',
+    venue: 'IEEE/ACM 46th International Conference on Software Engineering (ICSE)',
     year: 2024,
     doi: '10.1145/3597503.3639192'
   },
@@ -57,7 +57,7 @@ const publicationLinks: PublicationsLink[] = [
     title: 'Automated Detection of AI-Obfuscated Plagiarism in Modeling Assignments',
     authors: ['Timur Sağlam', 'Sebastian Hahner', 'Larissa Schmid', 'Erik Burger'],
     venue:
-      'IEEE/ACM 46th International Conference on Software Engineering: Software Engineering Education and Training (ICSE-SEET ’24)',
+      'IEEE/ACM 46th International Conference on Software Engineering: Software Engineering Education and Training (ICSE-SEET)',
     year: 2024,
     doi: '10.1145/3639474.3640084'
   },
@@ -66,7 +66,7 @@ const publicationLinks: PublicationsLink[] = [
     title: 'How Students Plagiarize Modeling Assignments',
     authors: ['Timur Sağlam', 'Larissa Schmid', 'Sebastian Hahner', 'Erik Burger'],
     venue:
-      '2023 ACM/IEEE International Conference on Model Driven Engineering Languages and Systems Companion (MODELS-C)',
+      'ACM/IEEE 26th International Conference on Model Driven Engineering Languages and Systems Companion (MODELS-C)',
     year: 2023,
     doi: '10.1109/MODELS-C59198.2023.00032'
   },
@@ -75,7 +75,7 @@ const publicationLinks: PublicationsLink[] = [
     title: 'Token-based plagiarism detection for metamodels',
     authors: ['Timur Sağlam', 'Sebastian Hahner', 'Jan Willem Wittler', 'Thomas Kühn'],
     venue:
-      '25th International Conference on Model Driven Engineering Languages and Systems: Companion Proceedings',
+      'ACM/IEEE 25th International Conference on Model Driven Engineering Languages and Systems Companion (MODELS-C)',
     year: 2022,
     doi: '10.1145/3550356.3556508'
   }
